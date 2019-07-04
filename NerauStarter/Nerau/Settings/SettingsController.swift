e20f60c64a499bc9725f68acf9a6c23fccc26714
Benedikt Terhechte <terhechte@gmail.com>
2019-07-04 14:12:05 +0200

    simplified

--- NerauStarter/Nerau/Settings/SettingsController.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-
-final class SettingsController: UITableViewController {
-    @IBAction func closeSettings(sender: Any) {
-        dismiss(animated: true, completion: nil)
-    }
-}
